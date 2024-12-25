3c1481f0fc06eda8ab577a75d6d355b418ee3d55
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-12-25 21:49:11 +0530

    socials update

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -8,27 +8,92 @@ const socials: Social[] = [
 	{
 		title: "Email",
 		icon: "carbon:email",
-		url: "mailto:lakshyajeetjalal@duck.com",
+		url: "mailto:lakshyajeet@jalal.uk.in",
+	},
+	{
+		title: "Website",
+		icon: "carbon:link",
+		url: "https://mglsj.eu.org/",
+	},
+	{
+		title: "LinkTree",
+		icon: "simple-icons:linktree",
+		url: "https://linktr.ee/mglsj",
 	},
 	{
 		title: "GitHub",
-		icon: "carbon:logo-github",
+		icon: "simple-icons:github",
 		url: "https://github.com/mglsj",
 	},
 	{
 		title: "LinkedIn",
-		icon: "carbon:logo-linkedin",
+		icon: "simple-icons:linkedin",
 		url: "https://www.linkedin.com/in/lakshyajeet-jalal/",
 	},
 	{
-		title: "Twitter",
-		icon: "carbon:logo-twitter",
+		title: "X / Twitter",
+		icon: "simple-icons:x",
 		url: "https://twitter.com/mg_lsj",
 	},
 	{
 		title: "Instagram",
-		icon: "carbon:logo-instagram",
+		icon: "simple-icons:instagram",
 		url: "https://www.instagram.com/mglsj.gg/",
+	},
+	{
+		title: "Reddit",
+		icon: "simple-icons:reddit",
+		url: "https://reddit.com/user/mglsj",
+	},
+	{
+		title: "Discord",
+		icon: "simple-icons:discord",
+		url: "https://discordapp.com/users/592915779662643240",
+	},
+	{
+		title: "Telegram",
+		icon: "simple-icons:telegram",
+		url: "https://t.me/mglsj",
+	},
+	{
+		title: "BlueSky",
+		icon: "simple-icons:bluesky",
+		url: "https://bsky.app/profile/mglsj.eu.org",
+	},
+	{
+		title: "GitLab",
+		icon: "simple-icons:gitlab",
+		url: "https://gitlab.com/mglsj/",
+	},
+	{
+		title: "Mastodon",
+		icon: "simple-icons:mastodon",
+		url: "https://mastodon.social/@mglsj",
+	},
+	{
+		title: "OpenStreetMap",
+		icon: "simple-icons:openstreetmap",
+		url: "https://www.openstreetmap.org/user/mglsj",
+	},
+	{
+		title: "Wikipedia",
+		icon: "simple-icons:wikipedia",
+		url: "https://en.wikipedia.org/wiki/User:Lakshyajeet_Jalal",
+	},
+	{
+		title: "npm",
+		icon: "simple-icons:npm",
+		url: "https://www.npmjs.com/~mglsj",
+	},
+	{
+		title: "PyPi",
+		icon: "simple-icons:pypi",
+		url: "https://pypi.org/user/mglsj/",
+	},
+	{
+		title: "pub.dev",
+		icon: "simple-icons:dart",
+		url: "https://pub.dev/publishers/mglsj.eu.org/",
 	},
 ];
 
