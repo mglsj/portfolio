81738f80ff718a8772526b8be1d45f80f28bef55
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2025-02-16 12:11:32 +0530

    socials update

--- src/data/about.ts
+++ src/data/about.ts
@@ -3,7 +3,6 @@ type About = {
 	name: string;
 	designation: string;
 	location: string;
-	website: string;
 	content: string;
 };
 
@@ -12,7 +11,6 @@ const about: About = {
 	name: "Lakshyajeet Singh Jalal",
 	designation: "Computer Science Student",
 	location: "Haldwani, Uttarakhand, IN",
-	website: "https://lakshyajeet.jalal.uk.in",
 	content:
 		"I am a Computer Science student at Graphic Era Hill University. I am passionate about software development, web technologies, and open-source. I enjoy learning new things and working on projects that challenge me to grow as a developer. I am always looking for opportunities to collaborate on interesting projects and connect with like-minded individuals. In my free time, I like to read books, watch movies, and play video games.",
 };
--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,105 +1,31 @@
 type Social = {
-	title: string;
-	icon: string;
+	id: string;
+	name: string;
 	url: string;
+	iconify: string;
 };
 
-const socials: Social[] = [
-	{
-		title: "Email",
-		icon: "carbon:email",
-		url: "mailto:lakshyajeet@jalal.uk.in",
-	},
-	{
-		title: "Website",
-		icon: "carbon:link",
-		url: "https://mglsj.eu.org/",
-	},
-	{
-		title: "LinkTree",
-		icon: "simple-icons:linktree",
-		url: "https://linktr.ee/mglsj",
-	},
-	{
-		title: "GitHub",
-		icon: "simple-icons:github",
-		url: "https://github.com/mglsj",
-	},
-	{
-		title: "LinkedIn",
-		icon: "simple-icons:linkedin",
-		url: "https://www.linkedin.com/in/mglsj",
-	},
-	{
-		title: "X / Twitter",
-		icon: "simple-icons:x",
-		url: "https://twitter.com/mg_lsj",
-	},
-	{
-		title: "Instagram",
-		icon: "simple-icons:instagram",
-		url: "https://www.instagram.com/mglsj.gg",
-	},
-	{
-		title: "Reddit",
-		icon: "simple-icons:reddit",
-		url: "https://reddit.com/user/mglsj",
-	},
-	{
-		title: "Discord",
-		icon: "simple-icons:discord",
-		url: "https://discordapp.com/users/592915779662643240",
-	},
-	{
-		title: "Telegram",
-		icon: "simple-icons:telegram",
-		url: "https://t.me/mglsj",
-	},
-	{
-		title: "BlueSky",
-		icon: "simple-icons:bluesky",
-		url: "https://bsky.app/profile/mglsj.eu.org",
-	},
-	{
-		title: "GitLab",
-		icon: "simple-icons:gitlab",
-		url: "https://gitlab.com/mglsj/",
-	},
-	{
-		title: "Mastodon",
-		icon: "simple-icons:mastodon",
-		url: "https://mastodon.social/@mglsj",
-	},
-	{
-		title: "OpenStreetMap",
-		icon: "simple-icons:openstreetmap",
-		url: "https://www.openstreetmap.org/user/mglsj",
-	},
-	{
-		title: "Wikipedia",
-		icon: "simple-icons:wikipedia",
-		url: "https://en.wikipedia.org/wiki/User:Lakshyajeet_Jalal",
-	},
-	{
-		title: "Play Store",
-		icon: "simple-icons:googleplay",
-		url: "https://play.google.com/store/apps/developer?id=MG-LSJ",
-	},
-	{
-		title: "npm",
-		icon: "simple-icons:npm",
-		url: "https://www.npmjs.com/~mglsj",
-	},
-	{
-		title: "PyPi",
-		icon: "simple-icons:pypi",
-		url: "https://pypi.org/user/mglsj/",
-	},
-	{
-		title: "pub.dev",
-		icon: "simple-icons:dart",
-		url: "https://pub.dev/publishers/mglsj.eu.org/",
-	},
-];
+const respone = await fetch(
+	"https://raw.githubusercontent.com/mglsj/socials/refs/heads/main/socials.json",
+).then((res) => res.json());
+
+const socials: Social[] = respone.primary as Social[];
+const socialsAlt: Social[] = respone.secondary as Social[];
+
+// const socials: Social[] = [
+// 	{
+//      id: "email",
+// 		title: "Email",
+// 		iconify: "carbon:email",
+// 		url: "mailto:lakshyajeet@jalal.uk.in",
+// 	},
+// 	{
+//      id: "website",
+// 		title: "Website",
+// 		iconify: "carbon:link",
+// 		url: "https://mglsj.eu.org/",
+// 	}
+// ];
 
 export default socials;
+export { socialsAlt };
