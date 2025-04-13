68669b6a1cf4efbdd8bc288f39e45431d029d3dd
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2025-04-13 22:12:12 +0530

    astro version bump

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
-
-import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://lakshyajeet.jalal.uk.in",
 	base: "/",
 	trailingSlash: "always",
-	integrations: [tailwind(), compress(), icon()],
+	integrations: [tailwind(), icon()],
 });
